535b14c7590844859cbcf88aaf58f56bccace29d
nianhui.qi <1948559620@qq.com>
2018-01-22 13:35:13 +0800

    引入页面切换loading功能

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="app">
+    <loading v-model="loadStatus"></loading>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import { Loading } from 'vux'
+  import { mapState } from 'vuex'
+  export default {
+    name: 'App',
+    components: {
+      Loading
+    },
+    computed: {
+      ...mapState({
+        loadStatus: state => state.loading.loadStatus
+      })
+    }
+  }
 </script>
 
 <style>
@@ -15,7 +26,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
